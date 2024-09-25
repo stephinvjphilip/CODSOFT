eaff3d724de863c12ae8ac54cf133e568c1ebade
Stephin V J <stephinvjphilip@gmail.com>
2024-09-25 22:40:11 +0530

    rest of the code added

--- Task_1/todo.py
+++ Task_1/todo.py
@@ -14,17 +14,68 @@ def main():
     print("4. Delete task")
     Print("5. Exit")
 
-    choice = input("Choose an option from 1 to 5")
+    choice = input("Choose an option from 1 to 5:")
 
     if choice == '1':
-        add_task()
+        description = input("Enter the task")
+        add_task(description)
     elif choice == '2':
         vp_task()
     elif choice == '3':
         vc_task()
     elif choice == '4':
-        del_task()
+        index = int(input("Enter the task number to delete:")) - 1
+        del_task(index)
     elif choice == '5':
         break
     else:
         print("Invalid Option")
+
+def load_tasks():
+    if os.path.exists(tasks_file):
+        with open(tasks_file, 'r') as file:
+            return json.load(file)
+    return []
+
+def save_tasks(tasks):
+    with open(tasks_file, 'w') as file:
+        json.dumb(tasks, file)
+
+def add_task(description):
+    tasks = load_tasks()
+    task = {"description": description, "completed": False}
+    tasks.append(task)
+    save_tasks(task)
+    print("Task added")
+
+def vp_task():
+    tasks = load_tasks()
+    print("\nPending Tasks:")
+    for index,  task in enumerate(tasks):
+        if not task['completed']:
+            print(f"{index + 1}.{task['description']}")
+
+def vc_task():
+    tasks = load_tasks()
+    print("\nCompleted Tasks:")
+    for index, task in enumerate(tasks):
+        if task['Completed']:
+            print(f"{index + 1}.{task['description']}")
+
+def complete_task(index):
+    tasks = load_tasks()
+    if 0 <= index < len(tasks):
+        tasks[index] ['completed'] = True
+        save_tasks(tasks)
+        print("Task Marked as completed")
+    else:
+        print("Invalid task number")
+
+def del_task(index):
+    tasks = load_tasks()
+    if 0 <= index < len(tasks):
+        removed_task = tasks.pop(index)
+        save_tasks(tasks)
+        print(f"Task'{removed_task['description']}' deleted")
+    else:
+        print("Invaild task number")
